68d4b9aeb04260a5af4ff9bf5fc2f93015421e73
zaap-repo <101466591+zaap-repo@users.noreply.github.com>
2022-09-11 05:43:12 -0700

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker_hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t suhail114/java-maven-app:java-mv-app-2.1 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push suhail114/java-maven-app:java-mv-app-2.1'
